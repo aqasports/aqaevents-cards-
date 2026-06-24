fc0eebaeee068ef67cfe3e19664b8f4b4a0db2da
Abdelmounaim <aqa.sports.academy@gmail.com>
2026-06-24 07:29:04 +0200

    chore: add redirect from eventcard to eventscard in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [],
   },
+  async redirects() {
+    return [
+      {
+        source: "/eventcard/:path*",
+        destination: "/eventscard/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
